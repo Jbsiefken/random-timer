a6ad72ec5bef0fe52e44d13dbd1e8b3b60f551cf
Josh Siefken <joshbsiefken@gmail.com>
2020-12-29 11:55:18 -0600

    adds enum Status

--- src/app/time-display/time-display.component.ts
+++ src/app/time-display/time-display.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeDisplayComponent implements OnInit {
 
+  formatted_time: string = "test";
+
   constructor() { }
 
   ngOnInit(): void {
