c9698c07058b0eb18457b7844e41c2437b6f273c
Josh Siefken <joshbsiefken@gmail.com>
2021-01-05 10:57:19 -0600

    adds (glitchy) time display

--- src/app/time-display/time-display.component.ts
+++ src/app/time-display/time-display.component.ts
@@ -10,7 +10,10 @@ export class TimeDisplayComponent implements OnChanges {
 
   @Input() status: Status;
   formatted_time: string;
-  status_message: string = "not yet started";
+  status_message: string;
+
+  elapsed_time: number = 0;
+  start_time: number;
 
   constructor() { }
 
@@ -25,10 +28,59 @@ export class TimeDisplayComponent implements OnChanges {
       break;
       case Status.on: {
         this.status_message = "on";
+        this.startDisplay();
       }
       break;
+    }
+  }
 
-      
+  startDisplay(){
+    this.start_time = Date.now();
+    /*
+    setTimeout(() => {
+      this.elapsed_time += (Date.now() - this.start_time);
+      console.log(Date.now());
+      console.log(this.elapsed_time);
+      this.formatTime(this.elapsed_time);
+    }, 1000);
+    */
+   
+   setInterval(() => {
+      this.elapsed_time += (Date.now() - this.start_time);
+    }, 10);
+    setInterval(() => {
+      this.formatTime(this.elapsed_time);
+    }, 100);
+    
+
+  }
+
+  //function called every interval that updates
+  //formatted_time based on elapsed_time
+  formatTime(time){
+    let hrsTime = time / 3600000;
+    let hrs = Math.floor(hrsTime);
+    let minTime = (hrsTime - hrs) * 60;
+    let min = Math.floor(minTime);
+    let secTime = (minTime - min) * 60;
+    let sec = Math.floor(secTime);
+    let msTime = (secTime - sec) * 100;
+    let ms = Math.floor(msTime);
+
+    let strHrs = hrs.toString().padStart(2, "0");
+    let strMin = min.toString().padStart(2, "0");
+    let strSec = sec.toString().padStart(2, "0");
+    let strMs = ms.toString().padStart(2, "0");
+
+    switch(hrs){
+      case 0: {
+        this.formatted_time = `${strMin}:${strSec}:${strMs}`;
+      }
+      break;
+      default: {
+        this.formatted_time = `${strHrs}:${strMin}:${strSec}:${strMs}`;
+      }
+      break;
     }
   }
 
