b079f1efeae03158fcea0634214eb46c51fcc7f0
Josh Siefken <joshbsiefken@gmail.com>
2021-01-21 09:18:24 -0600

    cleans up unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
-import { TimeDisplayComponent } from './time-display/time-display.component';
-import { TimeDisplayBravoComponent } from './time-display-bravo/time-display-bravo.component';
 import { ResultListComponent } from './result-list/result-list.component';
 import { FormattedTimeComponent } from './formatted-time/formatted-time.component';
 
@@ -14,8 +12,6 @@ import { FormattedTimeComponent } from './formatted-time/formatted-time.componen
   declarations: [
     AppComponent,
     TimerComponent,
-    TimeDisplayComponent,
-    TimeDisplayBravoComponent,
     ResultListComponent,
     FormattedTimeComponent
   ],
--- src/app/time-formatter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TimeFormatter } from './time-formatter';
-
-describe('TimeFormatter', () => {
-  it('should create an instance', () => {
-    expect(new TimeFormatter()).toBeTruthy();
-  });
-});
