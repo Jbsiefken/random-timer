d380cd9d3653a9c0fcebcdf4ad713268258234fa
Josh Siefken <joshbsiefken@gmail.com>
2021-02-09 16:00:34 -0600

    fixes import statement

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
-import { Status } from '../Status';
+import { Status } from '../status';
 
 @Component({
   selector: 'app-timer',
