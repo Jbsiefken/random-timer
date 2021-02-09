59d8f1564334a83c01d8d1b3b73d94afe86deee9
Josh Siefken <joshbsiefken@gmail.com>
2021-02-09 15:23:09 -0600

    cuts unnecessary js files

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
 
 const app = express();
-
-
-// import body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 
 // static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
 
-// Set our api routes
-const api = require('./server/routes/api');
-app.use('/api', api);
-
-
-// route all other paths to index
+// route paths to index
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
